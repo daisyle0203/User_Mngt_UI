6937351f9399aa1ef66d740ba19496993045394c
Daisy Le <daisy.le0203@gmail.com>
2023-04-15 17:08:20 -0500

    add google authentication

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,31 @@
+import { signOut, useSession } from "next-auth/react"
+import Image from "next/image"
 import React from "react"
 
 const Navbar = () => {
+  const { data: session, status } = useSession()
   return (
     <div className="bg-gray-800">
       <div className="h-16 px-8 flex items-center">
         <p className="text-white font-bold flex-auto">User Management System</p>
+        {session && (
+          <div className="flex items-center sm:space-x-2 justify-end">
+            <Image
+              className="rounded-full cursor-pointer"
+              src={session?.user?.image}
+              height={30}
+              width={30}
+              alt="User Image"
+            ></Image>
+            <p className="text-white font-bold">{session?.user.name}</p>
+            <button
+              onClick={() => signOut()}
+              className="rounded text-white font-semibold bg-red-500 hover:bg-red-700 hover:cursor-pointer px-6 py-2"
+            >
+              Sign Out
+            </button>
+          </div>
+        )}
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import AddUser from "@/components/AddUser"
+import Login from "@/components/Login"
 import Navbar from "@/components/Navbar"
+import { getSession } from "next-auth/react"
 import Head from "next/head"
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />
+
   return (
     <>
       <Head>
@@ -14,4 +18,12 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+// Function to get the session object
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+  return {
+    props: { session },
+  }
 }
